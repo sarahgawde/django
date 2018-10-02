c9aeb13f829b8b94ee611f1f5f7bac7b5f9639a9
sarahgawde <43248486+sarahgawde@users.noreply.github.com>
2018-10-02 13:29:21 +0530

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+ # -*- coding: utf-8 -*-
+from __future__ import unicode_literals 
+
+# Create your views here.
+
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+	
